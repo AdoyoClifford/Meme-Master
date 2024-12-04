0a5ff1b981bf5b37a3ef6f1f40ed9ea166cf1555
AdoyoClifford <adoyoclifford@gmail.com>
2024-12-04 23:46:05 +0300

    feat: Add home page layout
    Added the basic layout for the home page

---
+++ app/src/main/java/com/adoyo/mememaster/HomePage.kt
@@ -0,0 +1,80 @@
+package com.adoyo.mememaster
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.contentColorFor
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
+
+@Composable
+fun HomePage(modifier: Modifier = Modifier) {
+    Scaffold(
+        topBar = {
+            Text(text = "Your Memes")
+        },
+        floatingActionButton = {
+            modifier.clip(RoundedCornerShape(16.dp))
+            contentColorFor(
+                backgroundColor = Color(0xFFEADDFF)
+            )
+            IconButton(
+                onClick = { /*TODO*/ }
+
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = "Add",
+                    tint = Color(0xFF21005D)
+                )
+            }
+        },
+        modifier = modifier.padding(16.dp)
+    ) { innerPadding ->
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(R.drawable.meme)
+                    .build(),
+                contentDescription = null,
+                modifier = Modifier.size(200.dp)
+            )
+            Spacer(modifier = Modifier.padding(16.dp))
+            Text(
+                text = "Tap + button to create your first meme"
+            )
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomePagePreview() {
+    HomePage()
+}
